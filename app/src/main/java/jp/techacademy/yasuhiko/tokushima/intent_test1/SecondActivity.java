176c50b318412427ce6c9a5ca803df4611a9d6e9
koutaro <julianseed@gmail.com>
2016-09-07 00:53:26 +0900

    画面間で値を渡すところまで作成

--- app/src/main/java/jp/techacademy/yasuhiko/tokushima/intent_test1/SecondActivity.java
+++ app/src/main/java/jp/techacademy/yasuhiko/tokushima/intent_test1/SecondActivity.java
@@ -1,7 +1,9 @@
 package jp.techacademy.yasuhiko.tokushima.intent_test1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        int value1 = intent.getIntExtra("VALUE1", 0);
+        int value2 = intent.getIntExtra("VALUE2", 0);
+
+        TextView textview = (TextView) findViewById(R.id.textView);
+        textview.setText(String.valueOf(value1 + value2));
     }
 }
